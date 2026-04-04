bf3e66c9119ed8fad3935767d84b6d182c989014
svetik <sveta_dem06@mail.ru>
2026-04-04 12:53:17 +0300

    The notes were rendered on the musical stand and keyboard by clicking on the links in the menu. All that remains is to make the sound playback.

--- PianoTrainerApp/ViewModels/LessonsViewModel.cs
+++ PianoTrainerApp/ViewModels/LessonsViewModel.cs
@@ -2,24 +2,116 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Threading;
 
 namespace PianoTrainerApp.ViewModels
 {
-    public class LessonsViewModel
+    public class LessonsViewModel : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+ 
         public ObservableCollection<PianoKey> WhiteKeys { get; set; }
         public ObservableCollection<PianoKey> BlackKeys { get; set; }
+
+        // Выбранная нота
+        private string _selectedNote;
+        public string SelectedNote
+        {
+            get => _selectedNote;
+            set
+            {
+                _selectedNote = value;
+                OnPropertyChanged(nameof(SelectedNote));
+
+                UpdatePiano();
+                UpdateNotePosition();
+            }
+        }
+
+        // Символ для диеза (справа или слева от круга)
+        private string _notePrefixSharp;
+        public string NotePrefixSharp
+        {
+            get => _notePrefixSharp;
+            set { _notePrefixSharp = value; OnPropertyChanged(nameof(NotePrefixSharp)); }
+        }
+
+        // Символ для бемоля (справа от круга)
+        private string _notePrefixFlat;
+        public string NotePrefixFlat
+        {
+            get => _notePrefixFlat;
+            set { _notePrefixFlat = value; OnPropertyChanged(nameof(NotePrefixFlat)); }
+        }
+
+        // Показывать ли энгармоническую бемоль-ноту
+        private bool _showFlatNote;
+        public bool ShowFlatNote
+        {
+            get => _showFlatNote;
+            set { _showFlatNote = value; OnPropertyChanged(nameof(ShowFlatNote)); }
+        }
+
+        // Y позиция основной ноты
+        private double _noteY;
+        public double NoteY
+        {
+            get => _noteY;
+            set { _noteY = value; OnPropertyChanged(nameof(NoteY)); }
+        }
+
+        // Y позиция диез
+        public double NoteTextY => NoteY - 8;
+
+        // Y позиция бемоль-ноты
+        private double _flatNoteY;
+        public double FlatNoteY
+        {
+            get => _flatNoteY;
+            set { _flatNoteY = value; OnPropertyChanged(nameof(FlatNoteY)); }
+        }
+
+        // Y позиция бемоль
+        public double FlatNoteTextY => FlatNoteY - 12;
+
+        // Заголовок ноты для отображения справа
+        private string _noteHeader;
+        public string NoteHeader
+        {
+            get => _noteHeader;
+            set
+            {
+                _noteHeader = value;
+                OnPropertyChanged();
+            }
+        }
+
+        // Фраза рядом с нотным станом
+        private string _noteInstruction;
+        public string NoteInstruction
+        {
+            get => _noteInstruction;
+            set { _noteInstruction = value; OnPropertyChanged(nameof(NoteInstruction)); }
+        }
+
+        public bool IsNoteSelected(string note)
+        {
+            return SelectedNote == note;
+        }
         public LessonsViewModel()
         {
             WhiteKeys = new ObservableCollection<PianoKey>();
             BlackKeys = new ObservableCollection<PianoKey>();
 
             GenerateKeys();
-            SelectNote("C");
+
+            SelectedNote = "C";
+            NoteHeader = "До 1 октавы";
         }
 
         private void GenerateKeys()
@@ -61,5 +153,92 @@ namespace PianoTrainerApp.ViewModels
                 key.IsPressed = key.Note == note;
         }
 
+        // Метод обновления пианино
+        private void UpdatePiano()
+        {
+            foreach (var key in WhiteKeys.Concat(BlackKeys))
+                key.IsPressed = key.Note == SelectedNote;
+        }
+
+        // Метод обновления позиции ноты
+        private void UpdateNotePosition()
+        {
+            // Сбрасываем по умолчанию
+            NotePrefixSharp = "";
+            NotePrefixFlat = "";
+            ShowFlatNote = false;
+
+            switch (SelectedNote)
+            {
+                // Натуральные ноты
+                case "C": NoteY = 60; break;
+                case "D": NoteY = 52; break;
+                case "E": NoteY = 46; break;
+                case "F": NoteY = 39; break;
+                case "G": NoteY = 33; break;
+                case "A": NoteY = 26; break;
+                case "B": NoteY = 20; break;
+
+                // Диезы (слева #) и показываем бемоль-эквивалент
+                case "C#":
+                    NoteY = 60;
+                    NotePrefixSharp = "♯";
+                    ShowFlatNote = true;
+                    NotePrefixFlat = "♭";
+                    FlatNoteY = 52;
+                    break;
+                case "D#":
+                    NoteY = 52;
+                    NotePrefixSharp = "♯";
+                    ShowFlatNote = true;
+                    NotePrefixFlat = "♭";
+                    FlatNoteY = 46;
+                    break;
+                case "F#":
+                    NoteY = 39;
+                    NotePrefixSharp = "♯";
+                    ShowFlatNote = true;
+                    NotePrefixFlat = "♭";
+                    FlatNoteY = 33;
+                    break;
+                case "G#":
+                    NoteY = 33;
+                    NotePrefixSharp = "♯";
+                    ShowFlatNote = true;
+                    NotePrefixFlat = "♭";
+                    FlatNoteY = 26;
+                    break;
+                case "A#":
+                    NoteY = 26;
+                    NotePrefixSharp = "♯";
+                    ShowFlatNote = true;
+                    NotePrefixFlat = "♭";
+                    FlatNoteY = 20;
+                    break;
+            }
+
+            // Меняем тексты подсказок
+            if (ShowFlatNote)
+            {
+                NoteInstruction = "Энгармонически равные звуки\n- звучат одинаково, а пишутся по-разному";
+            }
+            else
+            {
+                NoteInstruction = "Сыграйте ноту самостоятельно,\nчтобы лучше её почувствовать!";
+            }
+
+            // Уведомляем UI о всех свойствах сразу
+            OnPropertyChanged(nameof(NoteY));
+            OnPropertyChanged(nameof(FlatNoteY));
+            OnPropertyChanged(nameof(NotePrefixSharp));
+            OnPropertyChanged(nameof(NotePrefixFlat));
+            OnPropertyChanged(nameof(ShowFlatNote));
+            OnPropertyChanged(nameof(NoteTextY));
+            OnPropertyChanged(nameof(FlatNoteTextY));
+        }
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
--- PianoTrainerApp/Views/LessonsView.xaml.cs
+++ PianoTrainerApp/Views/LessonsView.xaml.cs
@@ -44,5 +44,40 @@ namespace PianoTrainerApp.Views
                 grid.Clip = clipRect;
             }
         }
+
+        private void Note_Click(object sender, MouseButtonEventArgs e)
+        {
+            var tb = sender as TextBlock;
+            if (tb == null) return;
+
+            string note = "C";
+            string noteHeader = "До 1 октавы"; // заголовок по умолчанию
+
+            switch (tb.Text)
+            {
+                // Белые клавиши
+                case "До": note = "C"; noteHeader = "До 1 октавы"; break;
+                case "Ре": note = "D"; noteHeader = "Ре 1 октавы"; break;
+                case "Ми": note = "E"; noteHeader = "Ми 1 октавы"; break;
+                case "Фа": note = "F"; noteHeader = "Фа 1 октавы"; break;
+                case "Соль": note = "G"; noteHeader = "Соль 1 октавы"; break;
+                case "Ля": note = "A"; noteHeader = "Ля 1 октавы"; break;
+                case "Си": note = "B"; noteHeader = "Си 1 октавы"; break;
+
+                // Чёрные клавиши (диезы)
+                case "До#": note = "C#"; noteHeader = "До-диез / Ре-бемоль 1 октавы"; break;
+                case "Ре#": note = "D#"; noteHeader = "Ре-диез / Ми-бемоль 1 октавы"; break;
+                case "Фа#": note = "F#"; noteHeader = "Фа-диез / Соль-бемоль 1 октавы"; break;
+                case "Соль#": note = "G#"; noteHeader = "Соль-диез / Ля-бемоль 1 октавы"; break;
+                case "Ля#": note = "A#"; noteHeader = "Ля-диез / Си-бемоль 1 октавы"; break;
+            }
+
+            var vm = DataContext as LessonsViewModel;
+            if (vm != null)
+            {
+                vm.SelectedNote = note;
+                vm.NoteHeader = noteHeader; // сразу обновляем заголовок
+            }
+        }
     }
 }
